7eccbf1f33e2ecf7e7b41d61b43c841a35b7f72f
nausetjf <tuq71521@temple.edu>
2023-12-04 11:18:02 -0500

    Completed a basic hitbox and attack system and created a basic HUD

---
+++ godotgame/OpponentDamage.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $"../../../CharacterBody2D2" != null:
+		text = str($"../../../CharacterBody2D2".damage)
+	else:
+		text = "ded"
+	pass
--- godotgame/opponent.gd
+++ godotgame/opponent.gd
@@ -4,8 +4,32 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var damage = 0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _process(delta):
+	#$Sprite2D/Polygon2D.color = "white"
+	
+	if damage >= 4:
+		queue_free()
+	pass
+
+func hurt(directionhit,speedy):
+	#print("jndfsnjfsd")
+	$Sprite2D/Polygon2D.color = "blue"
+	velocity.y -= 1000 
+	position.y -= 20
+	damage += 1
+	if directionhit:
+		position.x += 30
+		velocity.x += 400/2
+	else:
+		position.x -= 30
+		velocity.x -= 400/2
+			
+	
 
 
 func _physics_process(delta):
@@ -17,6 +41,6 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	velocity.x /= SPEED
 	
-	
+
 	
 	move_and_slide()
