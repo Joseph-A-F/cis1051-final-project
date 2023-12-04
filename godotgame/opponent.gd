69d41ac7c64e0eed9045fa2bbd0c41655bfdeecb
nausetjf <tuq71521@temple.edu>
2023-12-04 11:18:02 -0500

    updates

--- godotgame/opponent.gd
+++ godotgame/opponent.gd
@@ -4,8 +4,34 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var damage = 0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _process(delta):
+	#$Sprite2D/Polygon2D.color = "white"
+	
+	if damage >= 4:
+		queue_free()
+	pass
+
+func hurt(directionhit,speedy):
+	#print("jndfsnjfsd")
+	$Sprite2D/Polygon2D.color = "blue"
+	velocity.y -= 1000 
+	position.y -= 20
+	damage += 1
+	if directionhit:
+		position.x += 50*damage
+		velocity.x += 400/2*damage
+	else:
+		position.x -= 50*damage
+		velocity.x -= 400/2*damage
+			
+	#camera
+	$"../Camera2D/AnimationPlayer".play("hit")
+	
 
 
 func _physics_process(delta):
@@ -17,6 +43,6 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	velocity.x /= SPEED
 	
-	
+
 	
 	move_and_slide()
